e0edc884a82adc118df00ea6b2cf61fe6f8622d6
Jake <test@fake.com.au>
2021-11-13 13:46:02 -0500

    Added vue.js front end. Confirmed working.

---
+++ Springboot-AWS-demo/src/frontend/src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+   <h1>{{message}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data(){
+    return {
+      message:""
+    }
+  },
+  mounted(){
+    fetch("/api/users")
+    .then((response)=>response.text())
+    .then((data)=>{
+      this.message = data;
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
